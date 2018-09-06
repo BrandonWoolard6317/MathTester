64ad9f8618c966446fbb65de197d847315bfc7d4
Brandon Woolard <brandon.woolard.2019@student.lhschools.org>
2018-09-06 10:14:53 -0400

    Just messing around

--- src/MathTester.java
+++ src/MathTester.java
@@ -1,23 +1,34 @@
+import java.util.Scanner;
+
 public class MathTester {
 
     public static void main(String[] args) {
         String firstWord;
         String secondWord;
-        int firstNumber,secondNumber,total;
+        String heightInches;
+        String personsHeight;
+        int firstNumber,secondNumber,total,inchesToFeet;
+        Scanner keyboard;
 
 
-
+        keyboard = new Scanner(System.in);
         firstWord = "dog";
         secondWord = "house";
         firstNumber = 5;
         secondNumber = 10;
+        inchesToFeet = (int) 0.0833333;
         total = firstNumber + secondNumber;
         System.out.println(total);
         total = total + 10;
         System.out.println(total);
 
+        /*System.out.print("What's your height in inches?");
+        heightInches = keyboard.nextLine();
+        personsHeight = heightInches + inchesToFeet;
+        System.out.println("Your height is " + (personsHeight));
+*/
 
-        
+
 
         /*System.out.println(firstWord + secondWord);
         System.out.println(firstWord + (firstNumber + secondNumber));
